5ee1f746009caf0cff5c5623a7d904aad870b8aa
dahall <dahall@kr.af.mil>
2020-07-15 16:30:29 -0400

    added game.js and readlineSync dependency.

---
+++ game.js
@@ -0,0 +1,16 @@
+// readlineSync Dependency - https://www.npmjs.com/package/readline-sync
+const readlineSync = require('readline-sync');
+//Character Selection
+
+//Get matched against another fighter
+
+//create a pool of fighters that are available
+
+// start a fight with a cpu fighter
+
+// match resolution
+
+// game resolution if won all matches 
+
+// turn clock for opponents during the round
+
