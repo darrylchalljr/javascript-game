261e035c88d136d880b6d15b4b0fdd46d4e7d795
dahall <dahall@kr.af.mil>
2020-07-21 16:32:20 -0400

    added array of fighters, moved some stuff around, removed testCharacters from setTimeout

--- game.js
+++ game.js
@@ -1,33 +1,9 @@
+/* Todos:
+1: Add validation logic to character selection.
+*/
+
 // readlineSync Dependency - https://www.npmjs.com/package/readline-sync
 const readlineSync = require('readline-sync');
-//Character Selection
-console.clear();
-
-console.log("Welcome to the arena!!!");
-
-// This is to delay the second message the user sees after joining the game.
-// setTimeout(
-//     () => {
-//         console.clear();
-//         console.log("Choose your fighter!");
-
-// Displays the names of the characters in the testCharacters Array.
-//         let numOfCharacters = testCharacters.length;
-//         for(i = 0; i < numOfCharacters; i += 1){
-//             console.log(testCharacters[i].name);
-//         }
-//         console.log("\n"); //adds a line break after the list of fighters.
-// //Gives the user the ability to type the name of the fighter they choose.
-//         let myCharacter = readlineSync.question("Fighter?: ")
-//     }, 
-//     3000
-//     );
-    
-    let testCharacters = [
-        {name: "John", description:"This is a sentence."},
-        {name: "Darryl", description:"This is a sentence."},
-        {name: "Daniel", description:"This is a sentence."}
-    ]
 
 //create a pool of fighters that are available
 class Attack {
@@ -58,9 +34,7 @@ class Fighter {
 
 let rufus = new Fighter("Rufus","A description of Rufus",100,rufusSpecial);
 let billy = new Fighter("Billy","This is billy.",100)
-console.log(rufus);
-console.log(billy);
-
+let availableFighters = [rufus,billy];
 //Get matched against another fighter
 
 // start a fight with a cpu fighter
@@ -70,4 +44,25 @@ console.log(billy);
 // game resolution if won all matches 
 
 // turn clock for opponents during the round
-    
+
+//Character Selection
+console.clear();
+
+console.log("Welcome to the arena!!!");
+
+// This is to delay the second message the user sees after joining the game.
+setTimeout(
+    () => {
+        console.clear();
+        console.log("Choose your fighter!");
+
+// Displays the names of the characters in the testCharacters Array.
+        for(i = 0; i < availableFighters.length; i += 1){
+            console.log(availableFighters[i].name);
+        }
+        console.log("\n"); //adds a line break after the list of fighters.
+//Gives the user the ability to type the name of the fighter they choose.
+        let myCharacter = readlineSync.question("Fighter?: ")
+    }, 
+    3000
+    );
