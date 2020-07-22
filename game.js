21f15c8eed9669a28fcf2e09f129766cd2eb30df
dsmall <dsmall.ctr@kr.af.mil>
2020-07-22 17:03:17 -0400

    working on start of fight logic

--- game.js
+++ game.js
@@ -1,9 +1,13 @@
 /* Todos:
-1: Add validation logic to character selection.
+1: Rename testFight to proper name
+2: Ensure figher selection gets passed into starting match correctly
+3: 
 */
+
 
 // readlineSync Dependency - https://www.npmjs.com/package/readline-sync
 const readlineSync = require('readline-sync');
+
 
 //create a pool of fighters that are available
 class Attack {
@@ -14,10 +18,13 @@ class Attack {
         this.coolDown = coolDown;
     }
 }
+
+
 // Common moves:
 const jab = new Attack("Jab","Punch",10,0);
 const lowKick = new Attack("Low Kick","Kick",12.5,0);
 const highKick = new Attack("High Kick","Kick",15,1);
+
 
 // Fighter-specific moves:
 const rufusSpecial = new Attack("Gut Punch", "Special",25,2);
@@ -38,8 +45,6 @@ let availableFighters = [rufus,billy];
 let validatedChar;
 //Get matched against another fighter
 
-// start a fight with a cpu fighter
-
 // match resolution
 
 // game resolution if won all matches 
@@ -52,40 +57,71 @@ console.clear();
 console.log("Welcome to the arena!!!");
 
 // This is to delay the second message the user sees after joining the game.
-setTimeout(
-    () => {
-        console.clear();
-        console.log("Choose your fighter!");
+// setTimeout(
+//     () => {
+//         console.clear();
+//         console.log("Choose your fighter!");
 
-// Displays the names of the characters in the testCharacters Array.
-        for(i = 0; i < availableFighters.length; i += 1){
-            console.log(availableFighters[i].name);
-        }
-        console.log("\n"); //adds a line break after the list of fighters.
+
+// // Displays the names of the characters in the testCharacters Array.
+//         for(i = 0; i < availableFighters.length; i += 1){
+//             console.log(availableFighters[i].name);
+//         }
+//         console.log("\n"); //adds a line break after the list of fighters.
         
         
-//Gives the user the ability to type the name of the fighter they choose.
-        function validateCharacter () {
-            function getCharacter (){
-                unvalidatedChar = readlineSync.question("Fighter?: ")
-            }        
-            getCharacter();
+// //Gives the user the ability to type the name of the fighter they choose.
+//         function validateCharacter () {
+//             function getCharacter (){
+//                 unvalidatedChar = readlineSync.question("Fighter?: ")
+//             }        
+//             getCharacter();
     
-            if (
-                availableFighters.find(
-                    (element) => {
-                        return (unvalidatedChar === element.name)
-                    }
-                )
-            ){
-                validatedChar = unvalidatedChar;
-            } else {
-                console.log("Please choose again!");
-                validateCharacter()
-            }
-            return validatedChar            
-        }
-        validateCharacter();
-},
-    1000
-);
+//             if (
+//                 availableFighters.find(
+//                     (element) => {
+//                         return (unvalidatedChar === element.name)
+//                     }
+//                 )
+//             ){
+//                 validatedChar = unvalidatedChar;
+//             } else {
+//                 console.log("Please choose again!");
+//                 validateCharacter()
+//             }
+//             return validatedChar            
+//         }
+//         validateCharacter();
+//         testFight(rufus,billy);
+// },
+//     1000
+// );
+
+
+//Start a fight with a cpu fighter
+testFight(rufus,billy);
+
+function testFight (validateCharacter, cpuOpponent) {
+    setTimeout (
+        () => {console.clear()},
+        1000
+    );
+    setTimeout (
+        () => {console.log(`${validateCharacter.name} enters the ring!`)},
+        2000
+    );
+    setTimeout (
+        () => {console.log(`${cpuOpponent.name} enters the ring!`)},
+        3000
+    );
+    console.log("Get ready to fight!");
+    // console.clear();
+    console.log("3");
+    console.log("2");
+    console.log("1");
+    // console.clear();
+}
+
+// Idea to solve recurring setTimeout problem in section above, use with a .forEach or .map loop
+let x = [console.clear(),console.log("test string")];
+console.log(x);
