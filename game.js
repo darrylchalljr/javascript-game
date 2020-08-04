b1a43b44bdbf8b649f41dfe61fcc91b6ed88059c
dahall <dahall@kr.af.mil>
2020-08-04 17:01:42 -0400

    created end fight sequence

--- game.js
+++ game.js
@@ -3,6 +3,7 @@
 2: Ensure figher selection gets passed into starting match correctly
 3: Account for complexity with cooldowns.
 4: Make sure we have annotations for big chunks of code.
+5: Convert numbers to words for damage and health e.g. 1-10 = low, 11-20 = moderate, etc.
 */
 
 
@@ -41,11 +42,12 @@ createCommonMove("Jab","Punch",10,0);
 createCommonMove("Low Kick","Kick",12.5,0);
 createCommonMove("High Kick","Kick",15,1);
 
+
 // create a pool of fighters that are available
 function createFighter (name,description,hp,...attacks){
     return availableFighters.push(new Fighter(name,description,hp,...attacks));
 };
-createFighter ("Rufus","A description of Rufus",105,rufusSpecial)
+createFighter ("Rufus","A description of Rufus",100,rufusSpecial)
 createFighter ("Billy","This is billy.",100)
 let rufus = availableFighters[0],
     billy = availableFighters[1],
@@ -58,6 +60,7 @@ function printFighterNames (fighterArray) {
     )
 };
 
+
 /*
 |---------------------------------------------------------------------|
 |                                                                     |
@@ -68,46 +71,46 @@ function printFighterNames (fighterArray) {
 
 // Get matched against another fighter
 
-console.log("Welcome to the arena!!!");
+// console.log("Welcome to the arena!!!");
 
-// This is to delay the second message the user sees after joining the game.
-setTimeout(
-    () => {
-        console.clear();
+// // This is to delay the second message the user sees after joining the game.
+// setTimeout(
+//     () => {
+//         console.clear();
         
-        console.log("Choose your fighter!");
+//         console.log("Choose your fighter!");
 
-        // Displays the names of the fighters.
-        printFighterNames(availableFighters);
+//         // Displays the names of the fighters.
+//         printFighterNames(availableFighters);
 
-        console.log("\n"); //adds a line break after the list of fighters.
+//         console.log("\n"); //adds a line break after the list of fighters.
 
-//Gives the user the ability to type the name of the fighter they choose.
-        function validateCharacter () {
-            function getCharacter (){
-                unvalidatedChar = readlineSync.question("Fighter?: ")
-            }        
-            getCharacter();
+// //Gives the user the ability to type the name of the fighter they choose.
+//         function validateCharacter () {
+//             function getCharacter (){
+//                 unvalidatedChar = readlineSync.question("Fighter?: ")
+//             }        
+//             getCharacter();
 
-            if (
-                availableFighters.find(
-                    (element) => {
-                        return (unvalidatedChar === element.name)
-                    }
-                )
-            ){
-                validatedChar = unvalidatedChar;
-            } else {
-                console.log("Please choose again!");
-                validateCharacter()
-            }
-            return validatedChar            
-        }
-        validateCharacter();
-        testFight(rufus,billy);
-},
-    1000
-);
+//             if (
+//                 availableFighters.find(
+//                     (element) => {
+//                         return (unvalidatedChar === element.name)
+//                     }
+//                 )
+//             ){
+//                 validatedChar = unvalidatedChar;
+//             } else {
+//                 console.log("Please choose again!");
+//                 validateCharacter()
+//             }
+//             return validatedChar            
+//         }
+//         validateCharacter();
+//         testFight(rufus,billy);
+// },
+//     1000
+// );
 
 
 //Start a fight with a cpu fighter
@@ -172,9 +175,25 @@ function testFight (validatedCharacter, cpuOpponent) {
         currentPlayerHealth -= cpuOpponent.moves[generatedCPUmove - 1].damage;
         currentCPUHealth -= validatedCharacter.moves[chosenPlayerMove - 1].damage;
         // Prints the damage done to both fighters.
-        console.log(`${cpuOpponent.name} hits ${validatedCharacter.name} with a ${cpuOpponent.moves[generatedCPUmove - 1].name} for ${cpuOpponent.moves[generatedCPUmove - 1].damage} damage.`
-        );
-        console.log(`${validatedCharacter.name} hits ${cpuOpponent.name} with a ${validatedCharacter.moves[chosenPlayerMove - 1].name} for ${validatedCharacter.moves[chosenPlayerMove - 1].damage} damage.`);
+                console.log(
+                    `${cpuOpponent.name} hits ${validatedCharacter.name} with a ${cpuOpponent.moves[generatedCPUmove - 1].name} for ${cpuOpponent.moves[generatedCPUmove - 1].damage} damage. ${validatedCharacter.name} has ${currentPlayerHealth} remaining.`)
+
+                console.log(
+                    `${validatedCharacter.name} hits ${cpuOpponent.name} with a ${validatedCharacter.moves[chosenPlayerMove - 1].name} for ${validatedCharacter.moves[chosenPlayerMove - 1].damage} damage. ${cpuOpponent.name} has ${currentCPUHealth} remaining.`)
+
+    };
+
+    // End the fight
+    function endFight(){
+        if (currentPlayerHealth <= 0 && currentCPUHealth <= 0){
+            console.log("Tie!");
+            
+        } else if (currentPlayerHealth <= 0) {
+            console.log("You lost! Game Over.");
+            
+        } else {
+            console.log("You won!");
+        }
     };
 
     let thingsToPrint = [
@@ -193,12 +212,19 @@ function testFight (validatedCharacter, cpuOpponent) {
 
     setTimeout (
         () => {
-            console.log("Choose your move: ");
-            printMoves(validatedCharacter);
-            userMove = readlineSync.question("");
-            validateMoveEntry(userMove);
-            chooseCPUMove(cpuOpponent.moves);
-            calcAndDisplayDamage(userMove,cpuOpponentMove);
+            do{
+                console.log("Choose your move: ");
+                printMoves(validatedCharacter);
+                userMove = readlineSync.question("");
+                validateMoveEntry(userMove);
+                chooseCPUMove(cpuOpponent.moves);
+                calcAndDisplayDamage(userMove,cpuOpponentMove);
+            } while(
+                (currentPlayerHealth > 0) 
+                && 
+                (currentCPUHealth > 0)
+                );
+            endFight();
         },
         fightDelay*(thingsToPrint.length+1)
     )
